3ccd6ef5cae929c735232f5cd2d9604de4038204
bhupendra-negi <bhupendra.negi1011@gmail.com>
2016-03-21 23:41:38 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// this module needs to be refrences in html page
+var myApp  = angular.module("myModule",[]);
+// attaching controller to create momdel for view to show it
+myApp.controller("myController",function($scope)
+{
+  $scope.message = "My first Angular Model";
+  // object in Model
+  var employee = {
+    firstName : "bhupem",
+    secondName : "negi"
+  }
+  $scope.employee = employee;
+})
+
+// another controller to load images with ng-src directive
+myApp.controller("ImgController", function($scope){
+
+var country  = {
+  name:"INDIA",
+  capital:"New Delhi",
+  image:"images/flag.png"
+};
+// attaching country model to $scope.
+$scope.country = country;
+});
